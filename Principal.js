e3c43986d3494bfe5b7b9944cad41d1342ce04f9
Camilo Ocampo <juan2411ocam.com>
2019-05-27 17:03:25 -0500

    Version 2.0

--- Principal.js
+++ Principal.js
@@ -1,60 +1,70 @@
 const fs = require('fs');
 const {cursos} = require ('./Cursos');
 const estudiante = require ('./Estudiante');
-
+const express = require('express')
+const app = express()
 const argv = require('yargs')
 .command('inscribir','matricular un curso',estudiante)
 .argv;
-if(arvg == undefined){
 
-    let Recorrer = (cursos,callback) => {
-        cursos.forEach(element => {
-            setTimeout(function() {
-                let result = 'el curso: '+element.nombre+' con el id: '+element.id+' es de: '+element.duracion+'h y cuesta: '+ element.valor + '\n';
-                callback (result);
-            }, 2000); 
-        });
-    };
-    
-    
-    Recorrer(cursos,function(result){
-        console.log(result);
+var result ="";
+ 
+app.listen(3000)
+
+
+let Recorrer = (cursos,callback) => {
+    cursos.forEach(element => {
+             result = 'el curso: '+element.nombre+' con el id: '+element.id+' es de: '+element.duracion+'h y cuesta: '+ element.valor + '\n';
+            callback (result);
     });
-    
-}else{
+};
+
+
+
 
 var id = cursos.find(function(element){
-    // cursos.forEach(element => {
-    //      if(element.id == argv.id){
-    //         return argv.id == element.id;
-    //      }
-    // });
     return argv.id == element.id;
 });
 
 function crearArchivo(arvg){
 if(id == undefined){
     console.log('el id no corresponde a ningun curso\n');
-    // Recorrer(cursos,function(result){
-    //     console.log(result);
-    // });
+    Recorrer(cursos,function(result){
+        setTimeout(function() {
+        console.log(result);
+    }, 2000); 
+    });
 }else{
     cursos.forEach(element => {
         if (element.id == arvg.id) {
             var txt = 'el estudiante: ' + argv.nombre + '\n' +
                     'con la cedula: ' + argv.cedula + '\n' + 
                     'esta matriculado en el curso de: '+ element.nombre;
-            fs.writeFile('Estudiantes.txt',txt,(err)=>{
-                if(err) throw (err);
-                console.log('archivo creado correctamente');
-            });
+                    app.get('/', function (req, res) {
+                        res.send(txt);
+                      });
+            // fs.writeFile('Estudiantes.txt',txt,(err)=>{
+            //     if(err) throw (err);
+            //     console.log('archivo creado correctamente');
+            // });
         }
     });
 }
 }
 
-crearArchivo(argv);
-
-
+if(argv._[0] == 'inscribir'){
+    crearArchivo(argv);
+}else{
+    Recorrer(cursos,function(result){
+        setTimeout(function() {
+        console.log(result);
+    }, 2000); 
+    });
 }
+
+
+
+
+
+
 
